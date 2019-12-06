ac1f1eac7c1fe031f100d1aa67c89c0cf7bfd156
Rafael Matias <rafael@skyle.net>
2019-12-06 15:14:37 +0100

    add dns publishing via cloudflare

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ CRAWL_GIT_EMAIL="${CRAWL_GIT_EMAIL:-crawler@localhost}"
 
 CRAWL_TIMEOUT="${CRAWL_TIMEOUT:-30m}"
 CRAWL_DNS_SIGNING_KEY="${CRAWL_DNS_SIGNING_KEY:-/secrets/key.json}"
+CRAWL_DNS_PUBLISH="${CRAWL_DNS_PUBLISH-false}"
+CLOUDFLARE_API_TOKEN="${CLOUDFLARE_API_TOKEN-}"
+CLOUDFLARE_ZONE_ID="${CLOUDFLARE_ZONE_ID-}"
 
 set -xe
 
@@ -25,27 +28,33 @@ generate_list() {
   devp2p discv4 crawl -timeout "$CRAWL_TIMEOUT" all.json
 
   # Mainnet: All nodes
-  mkdir -p all.mainnet.nodes.ethereum.org
-  devp2p nodeset filter all.json -eth-network mainnet > all.mainnet.nodes.ethereum.org/nodes.json
+  mkdir -p all.mainnet.nodes.ethflare.xyz
+  devp2p nodeset filter all.json -eth-network mainnet > all.mainnet.nodes.ethflare.xyz/nodes.json
 
   # Mainnet: LES nodes
-  mkdir -p les.mainnet.nodes.ethereum.org
-  devp2p nodeset filter all.json -eth-network mainnet -les-server > les.mainnet.nodes.ethereum.org/nodes.json
-
-  rm all.json
+  mkdir -p les.mainnet.nodes.ethflare.xyz
+  devp2p nodeset filter all.json -eth-network mainnet -les-server > les.mainnet.nodes.ethflare.xyz/nodes.json
 }
 
 sign_lists() {
-  for D in *.nodes.ethereum.org; do
+  for D in *.nodes.ethflare.xyz; do
     if [ -d "${D}" ]; then
       echo "" | devp2p dns sign "${D}" "$CRAWL_DNS_SIGNING_KEY"
     fi
   done
 }
 
+publish_dns() {
+  for D in *.nodes.ethflare.xyz; do
+    if [ -d "${D}" ]; then
+      devp2p dns to-cloudflare -zoneid "$CLOUDFLARE_ZONE_ID" "${D}"
+    fi
+  done
+}
+
 git_push() {
   if [ -n "$(git status --porcelain)" ]; then
-    git add ./*.nodes.ethereum.org/nodes.json
+    git add all.json ./*.nodes.ethflare.xyz/*.json
     git commit --message "automatic update: crawl time $CRAWL_TIMEOUT"
     git push origin "$CRAWL_GIT_BRANCH"
   fi
@@ -56,10 +65,15 @@ git_push() {
 git_setup
 generate_list
 
+
+if [ -f "$CRAWL_DNS_SIGNING_KEY" ]; then
+  sign_lists
+fi
+
 if [ "$CRAWL_GIT_PUSH" = true ] ; then
   git_push
 fi
 
-if [ -f "$CRAWL_DNS_SIGNING_KEY" ]; then
-  sign_lists
+if [ "$CRAWL_DNS_PUBLISH" = true ] ; then
+  publish_dns
 fi
